2945ac546211b6b9c5e123708d238328df8a9746
Abarna440 <prabarna2006@gmail.com>
2025-09-29 15:16:31 +0530

    Add files via upload

---
+++ GreenQuest.jsx
@@ -0,0 +1,9 @@
+/* 
+  GreenQuest - Gamified Learning for Greenskilling Awareness
+  Project: Naan Mudhalvan
+  (See canvas for full implementation)
+  ----------------------------------------------------------
+  Copy this file into `src/GreenQuest.jsx` of your React project.
+*/
+// Full code is already in the canvas. Keeping file as reference.
+
